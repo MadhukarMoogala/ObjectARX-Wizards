55a02d2c9774591de49093b459809cdd406ac68b
Madhukar Moogala <madhukar.moogala@autodesk.com>
2015-04-07 10:42:12 +0530

    ObjectARX Wizard for ACAD 2016
    Test:
    Migrated wizards from ACAD 15

---
+++ ObjectARX-Wizards-ForAutoCAD2016/ObjectARXWizardsInstaller/install.bat
@@ -0,0 +1,1 @@
+msiexec /i  C:\Users\moogalm\Downloads\ObjectARX-Wizards-ForAutoCAD2016\ObjectARXWizardsInstaller\ObjectARXWizards.msi
---
+++ ObjectARX-Wizards-ForAutoCAD2016/ObjectARXWizardsInstaller/make.bat
@@ -0,0 +1,14 @@
+@echo off
+%~d0
+cd /d %~dp0
+
+if exist w:\nul subst w: /d
+subst w: "C:\Program Files (x86)\WiX Toolset v3.8"
+
+w:\bin\candle.exe ObjectARXWizards.wxs -out temp\ObjectARXWizards.wixobj
+w:\bin\light.exe -sw1076 -b .. temp\ObjectARXWizards.wixobj -out ObjectARXWizards.msi 
+rem -ext WixUIExtension
+if exist ObjectARXWizards-2016.zip del ObjectARXWizards-2016.zip > nul
+"C:\Program Files\WinRAR\WinRAR.exe" a ObjectARXWizards-2016.zip ObjectARXWizards.msi > nul
+
+pause
